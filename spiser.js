ce28767131af874bb825a4eb21f1c3eded0f93af
Joe Gibbs Politz <joe.politz@gmail.com>
2021-08-03 15:45:54 -0700

    add test chats

--- spiser.js
+++ spiser.js
@@ -2,8 +2,17 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+chats = [
+    "Joe says hi",
+    "Another test"
+];
+
 app.get('/', (request, response) => {
-  response.send('Hello World!');
+    chatString = "";
+    for(var c of chats) {
+        chatString += c + "\n";
+    }
+    response.send(chatString);
 });
 
 app.listen(port, () => {
